346021e84a9b43b72f0a0b615bb2e372d77976f8
Michael Deal <mudcube@gmail.com>
2012-11-11 17:28:26 -0800

    add libraries: DOMLoader.script, *.link, *.image, and *.XMLHttp

---
+++ js/Window/DOMLoader.link.js
@@ -0,0 +1,30 @@
+/*
+	DOM ELEMENT CALLBACKS
+	--------------------------- 
+	DOMLoader.link({
+		href: "./fonts/Ultima.css",
+		fontFamily: "Ultima", // if checking for <canvas>
+		callback: function() { }
+	});
+*/
+
+if (typeof(DOMLoader) === "undefined") var DOMLoader = {};
+
+(function() { "use strict";
+
+DOMLoader.link = function(conf) {
+	if (typeof(conf) === "string") conf = { href: conf };
+	var hash = conf.href.replace(/[^a-zA-Z 0-9]+/g, "");
+	if (!document.getElementById(hash)) {
+		var link = document.createElement("link");
+		link.href = conf.href;
+		link.id = hash;
+		link.setAttribute("rel", "stylesheet");
+		link.setAttribute("type", "text/css");
+		var head = document.getElementsByTagName("head")[0];
+		if (!head) head = document.body;
+		head.appendChild(link);
+	}
+};
+
+})();
